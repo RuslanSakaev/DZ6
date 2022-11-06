f2fd77cda7c5ffb7bd9fc63e2800db44e60921ca
RuslanSakaev <hardist@mail.ru>
2022-11-07 01:41:38 +0300

    Добавил решение задачи 43

--- DZ43/Program.cs
+++ DZ43/Program.cs
@@ -1,18 +1,68 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) PointIntersectionLines
 
-Console.WriteLine("введите значение b1");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k1");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите значение b2");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k2");
-double k2 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Заданы значения прямых: ");
 
-double x = (-b2 + b1)/(-k1 + k2);
-double y = k2 * x + b2;
+double[,] arrayPointsLine = new double[2, 2];
+double[] arrayPointsLineIntersection = new double[2];
 
-Console.WriteLine($"точка пересечения двух прямых со значениями b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}, {y})");
+
+
+FillArray();
+PointIntersectionLines(arrayPointsLine);
+PrintArray(arrayPointsLine);
+
+
+void FillArray()
+{
+    for (int i = 0; i < arrayPointsLine.GetLength(0); i++)
+    {
+        Console.WriteLine($"Введите значение для {i + 1}-ой прямой: ");
+        for (int j = 0; j < arrayPointsLine.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"k: ");
+            else Console.Write($"b: ");
+            arrayPointsLine[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+
+double[] CompareLines(double[,] arrayPointsLine)
+{
+    arrayPointsLineIntersection[0] = (arrayPointsLine[1, 1] - arrayPointsLine[0, 1]) / (arrayPointsLine[0, 0] - arrayPointsLine[1, 0]);
+    arrayPointsLineIntersection[1] = arrayPointsLineIntersection[0] * arrayPointsLine[0, 0] + arrayPointsLine[0, 1];
+    return arrayPointsLineIntersection;
+}
+
+void PointIntersectionLines(double[,] arrayPointsLine)
+{
+    if (arrayPointsLine[0, 0] == arrayPointsLine[1, 0] && arrayPointsLine[0, 1] == arrayPointsLine[1, 1])
+    {
+        Console.Write($"Прямые совпадают");
+    }
+    else if (arrayPointsLine[0, 0] == arrayPointsLine[1, 0] && arrayPointsLine[0, 1] != arrayPointsLine[1, 1])
+    {
+        Console.Write($"Прямые параллельны");
+    }
+    else
+    {
+        CompareLines(arrayPointsLine);
+        Console.Write($"Точка пересечения прямых ->({arrayPointsLineIntersection[0]}, {arrayPointsLineIntersection[1]})\n");
+    }
+}
+
+void PrintArray(double[,] arrayPointsLine)
+{
+    for (int i = 0; i < arrayPointsLine.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < arrayPointsLine.GetLength(1); j++)
+        {
+            if (j < arrayPointsLine.GetLength(1) - 1) Console.Write($"{arrayPointsLine[i, j], 1}, ");
+            else Console.Write($"{arrayPointsLine[i, j], 1} ");
+        }
+        Console.WriteLine(" ");
+    }
+}
