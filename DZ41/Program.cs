a4c7e2dc71572e8ecd549d15466fed400ff0387a
RuslanSakaev <hardist@mail.ru>
2022-11-06 23:00:56 +0300

    Исправил решение DZ41, добавил try, catch

--- DZ41/Program.cs
+++ DZ41/Program.cs
@@ -3,29 +3,36 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите числа через пробел: ");
-
-double[] numbers = Array.ConvertAll(Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), double.Parse);
-
-double StringNumSum(double[] numbers)
+try
 {
-    int count = 0;
-    for (int i = 0; i < numbers.Length; i++)
+    Console.Write("Введите числа через пробел: ");
+
+    double[] numbers = Array.ConvertAll(Console.ReadLine().Split(new char[] {' ', '.', ':' }, StringSplitOptions.RemoveEmptyEntries), double.Parse);
+
+    double StringNumSum(double[] numbers)
     {
-        if (numbers[i] > 0) count += 1;
+        int count = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > 0) count += 1;
+        }
+        return count;
     }
-    return count;
+
+    PrintArray(numbers);
+
+    void PrintArray(double[] array)
+    {
+        Console.Write("Введены числа: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+            else Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine($" -> чисел больше 0 -> {StringNumSum(numbers)}");
 }
-
-PrintArray(numbers);
-
-void PrintArray(double[] array)
+catch (SystemException)
 {
-    Console.Write("Введены числа: ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
-        else Console.Write($"{array[i]}");
-    }
+    Console.WriteLine("Во время выполнения программы произошла ошибка, вероятно, были введены некорректные данные!");
 }
-Console.WriteLine($" -> чисел больше 0 -> {StringNumSum(numbers)}");
